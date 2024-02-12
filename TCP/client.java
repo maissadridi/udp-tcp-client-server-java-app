35ab73a94916d6374aec00be5ec0837bf21df066
Maissa Dridi <maissadridi1@gmail.com>
2024-02-12 16:55:06 +0100

    tcp calculator multiplication of two ints

--- TCP/client.java
+++ TCP/client.java
@@ -1,17 +1,32 @@
 package TCP;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class client {
     public static void main(String[] args) throws Exception {
-        String sentence = "Hello, server!";
-        Socket clientSocket = new Socket("localhost", 6789);
+        String serverAddress = "localhost";
+        int serverPort = 6790;
 
-        PrintWriter outToServer = new PrintWriter(clientSocket.getOutputStream(), true);
-        outToServer.println(sentence);
+        Socket clientSocket = new Socket(serverAddress, serverPort);
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        System.out.print("Enter the first number: ");
+        int num1 = Integer.parseInt(inFromUser.readLine());
+        System.out.print("Enter the second number: ");
+        int num2 = Integer.parseInt(inFromUser.readLine());
+
+        outToServer.writeBytes(num1 + " " + num2 + '\n');
+
+        String serverResponse = inFromServer.readLine();
+        System.out.println("Server response: " + serverResponse);
 
         clientSocket.close();
     }
-
 }
--- TCP/server.java
+++ TCP/server.java
@@ -1,19 +1,27 @@
 package TCP;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class server {
     public static void main(String[] args) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(6789);
+        ServerSocket serverSocket = new ServerSocket(6790);
 
         while (true) {
             Socket connectionSocket = serverSocket.accept();
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            String clientSentence = inFromClient.readLine();
-            System.out.println("Received: " + clientSentence);
+            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+
+            String clientNumbers = inFromClient.readLine();
+            String[] numbers = clientNumbers.split(" ");
+            int num1 = Integer.parseInt(numbers[0]);
+            int num2 = Integer.parseInt(numbers[1]);
+            int result = num1 * num2;
+            System.out.println("result from server: " + result);
+            outToClient.writeBytes(String.valueOf(result));
         }
     }
 }
